a868e5fc22ad9a25a7f7ba10d1c20d0b7850cebe
caio-lcf <cardosocaio032@gmail.com>
2024-07-26 00:50:35 -0300

    final version

--- first-project/src/main/java/com/caiospring/first_project/HelloController.java
+++ first-project/src/main/java/com/caiospring/first_project/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/SpringCaio")
-public class HelloController {
+public class HelloController  {
 
     @GetMapping
     public String hello() {
