2ccff839ab5ca1c67e7ff6c2c8eed916a7823e9b
caio-lcf <cardosocaio032@gmail.com>
2024-07-26 21:35:35 -0300

    localhost alterado

--- first-project/src/main/java/com/caiospring/first_project/HelloController.java
+++ first-project/src/main/java/com/caiospring/first_project/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/SpringCaio")
-public class HelloController  {
+@RequestMapping ("/")
+public class HelloController {
 
     @GetMapping
     public String hello() {
